a9d71909c724eae42905f0ba517eb0e8a68a510e
Linux User <adrien.candiotti@gmail.com>
2016-03-25 13:05:22 +0000

    mod - modified to listen events on docker socket instead of dockercloud API

--- server.py
+++ server.py
@@ -6,7 +6,8 @@ import logging
 import etcd
 from docker import Client
 
-cli = Client(base_url='unix://var/run/docker.sock')
+cli = Client(base_url='unix:///var/run/docker.sock')
+
 events = cli.events(decode=True)
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,21 +15,21 @@ logging.basicConfig(level=logging.DEBUG)
 etcd_hostname = 'etcd'
 etcd_client = etcd.Client(host=etcd_hostname)
 
-def on_open():
-    logging.warning('Connection inited with docker cloud api')
-
-def on_close():
-    logging.warning('Shutting down')
-
 def get_container(message):
-    uri = message.get('resource_uri').split('/')[-2]
-    return dockercloud.Container.fetch(uri)
+    container = message.get('Actor')
+    container['Attributes']['ID'] = container['ID']
+    return container['Attributes']
 
 def get_envvar(container, to_find):
-    for envvar in container.container_envvars:
-        if envvar['key'] == to_find:
-            return envvar['value']
+    container_details = cli.inspect_container(container['ID'])
+    env = container_details['Config']['Env']
+    for envvar in env:
+      if envvar.startswith(to_find + '='):
+       return envvar.split('=')[1]
     return None
+
+def get_container_hostname(container):
+    return container['name'] + '.weave.local'
 
 def create_backend(backend_name):
     key = '/vulcand/backends/%s/backend' % backend_name
@@ -38,18 +39,7 @@ def create_backend(backend_name):
     except etcd.EtcdKeyNotFound:
         value = '{"Type": "http"}'
         etcd_client.write(key, value)
-        logging.warning('Created backend : %s' % key)
-        return False
-
-def add_https_redirect(backend_name):
-    key = '/vulcand/frontends/%s/middlewares/http2https' % backend_name
-    try:
-        etcd_client.read(key)
-        return True
-    except etcd.EtcdKeyNotFound:
-        value = '{"Type": "rewrite", "Middleware":{"Regexp": "^http://(.*)$", "Replacement": "https://$1", "Redirect": true}}'
-        etcd_client.write(key, value)
-        logging.warning('Added https redirect middleware : %s' % key)
+        logging.info('Created backend : %s' % key)
         return False
 
 def create_frontend(backend_name, ROUTE):
@@ -63,72 +53,58 @@ def create_frontend(backend_name, ROUTE):
         value = '{"Type": "http", "BackendId": "%s", "Route": "PathRegexp(`%s.*`)"}'\
                 % (backend_name, ROUTE)
         etcd_client.write(key, value)
-        logging.warning('Created frontend : %s' % key)
+        logging.info('Created frontend : %s' % key)
+        return False
+
+def remove_frontend(backend_name):
+    key = '/vulcand/frontends/%s/frontend' % backend_name
+    try:
+        etcd_client.read(key)
+        return True
+    except etcd.EtcdKeyNotFound:
+        etcd_client.delete(key)
+        logging.info('Removed frontend : %s' % key)
         return False
 
 def add_container(container):
-    server_name = container.name
+    server_name = container['name']
 
-    ROUTE = get_envvar(container, 'ROUTE')
+    ROUTE = '/' + server_name
 
-    if not ROUTE:
-        logging.warning('No route found for container: ' + server_name)
-        return
-
-    backend_name = server_name.split('-')[0]
+    backend_name = server_name
     create_backend(backend_name)
 
     HOSTNAME = get_container_hostname(container)
     PORT = get_envvar(container, 'PORT')
     ROUTE = get_envvar(container, 'ROUTE')
 
+    logging.info('Adding: %s:%s%s' % (HOSTNAME, PORT, ROUTE))
+
     if PORT:
+        if not ROUTE:
+          logging.info('No route could be found for this container' + server_name)
+          return
+
         key = '/vulcand/backends/%s/servers/%s' % (backend_name, server_name)
         value = '{"URL": "http://%s:%s"}' % (HOSTNAME, PORT)
 
         etcd_client.write(key, value)
-        logging.warning('Added server: %s = %s on route %s' % (key, value, ROUTE))
+        logging.info('Added server: %s = %s on route %s' % (key, value, ROUTE))
         create_frontend(backend_name, ROUTE)
-        add_https_redirect(backend_name)
     else:
-        logging.warning('No port could be found for this container' + container_name)
+        logging.info('No port could be found for this container' + server_name)
 
 def remove_container(container):
-    server_name = container.name
-    backend_name = server_name.split('-')[0]
+    server_name = container['name']
+    backend_name = server_name
 
     key = '/vulcand/backends/%s/servers/%s' % (backend_name, server_name)
     try:
         etcd_client.delete(key)
-        logging.warning('Removed server: %s' % key)
+        logging.info('Removed server: %s' % key)
+        remove_frontend(backend_name)
     except etcd.EtcdKeyNotFound as e:
         logging.error(e)
-
-def on_message(message):
-    message = json.loads(message)
-
-    if 'type' in message:
-        if message['type'] == 'container':
-            if 'action' in message:
-                if message['action'] == 'update':
-                    if message['state'] == 'Running':
-                        logging.warning('Running')
-                        container = get_container(message)
-                        add_container(container)
-
-                    elif message['state'] == 'Stopped': 
-                        logging.warning('Stopped')
-                        container = get_container(message)
-                        remove_container(container)
-
-                elif message['action'] == 'delete':
-                      if message['state'] == 'Terminated':
-                        logging.warning('Terminated')
-                        container = get_container(message)
-                        remove_container(container)
-
-def on_error(error):
-    logging.error(error)
 
 def create_listener(name, protocol, address):
     key = '/vulcand/listeners/%s' % name
@@ -138,10 +114,18 @@ def create_listener(name, protocol, address):
         value = '{"Protocol":"%s", "Address":{"Network":"tcp", "Address":"%s"}}' % (protocol, address)
         etcd_client.write(key, value)
 
-event_manager.on_error(on_error)
-event_manager.on_message(on_message)
-
 create_listener('http', 'http', "0.0.0.0:80")
 
 for event in events:
-    print event
+	print(event)
+	action = event['Action']
+	container = get_container(event)
+
+	if action == 'stop':
+            logging.info('Stopped')
+            remove_container(container)
+	elif action == 'start':
+            logging.info('Started')
+            add_container(container)
+	else:
+	    pass
